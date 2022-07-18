dae2dda63504d5378f19d817adecab9dbf392056
devrayat000 <2010050@me.buet.ac.bd>
2022-07-19 01:18:57 +0600

    setup for netlify

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverBuildTarget:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL ? "netlify" : "node-cjs",
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.js"
+      : undefined,
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",

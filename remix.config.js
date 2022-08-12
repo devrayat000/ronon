5f190142a074c7012ab6c6c3eb16e93e8236ac33
devrayat000 <2010050@me.buet.ac.bd>
2022-08-12 09:58:13 +0600

    implemented vercel compat

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,16 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   serverBuildTarget:
-    process.env.NETLIFY || process.env.NETLIFY_LOCAL ? "netlify" : "node-cjs",
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "netlify"
+      : process.env.VERCEL || process.env.VERCEL_URL
+      ? "vercel"
+      : "node-cjs",
   server:
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
-      ? "./server.js"
+      ? "./server.netlify.js"
+      :process.env.VERCEL || process.env.VERCEL_URL
+      ? "./server.vercel.js"
       : undefined,
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
